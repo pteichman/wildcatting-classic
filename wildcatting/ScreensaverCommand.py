658122d77afcbc87286143627761e4bed0b76bc6
Peter Teichman <peter@teichman.org>
2007-10-04 20:32:13 +0000

    add --no-border

--- wildcatting/ScreensaverCommand.py
+++ wildcatting/ScreensaverCommand.py
@@ -12,26 +12,34 @@ class ScreensaverCommand(Command):
     def __init__(self):
         Command.__init__(self, "screensaver", summary="Run the Wildcatting screensaver")
 
+        self.add_option("", "--no-border", action="store_true",
+                        help="disable border")
+
         self.y_border = 2
         self.x_border = 3
 
-    def cursesScreensaver(self, stdscr):
+    def cursesScreensaver(self, stdscr, options, args):
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_RED)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)
         curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_YELLOW)
         curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLUE)
         curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_MAGENTA)
         curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_CYAN)
-        
-        screen_h, screen_w = stdscr.getmaxyx()
-        win = stdscr.derwin(screen_h-self.y_border*2,
-                            screen_w-self.x_border*2,
-                            self.y_border, self.x_border)
+
+        if options.no_border:
+            win = stdscr
+            border = 0
+        else:
+            screen_h, screen_w = stdscr.getmaxyx()
+            win = stdscr.derwin(screen_h-self.y_border*2,
+                                screen_w-self.x_border*2,
+                                self.y_border, self.x_border)
     
-        win.box()
+            win.box()
+            border = 1
 
         win_h, win_w = win.getmaxyx()
-        win = win.derwin(win_h-2, win_w-2, 1, 1)
+        win = win.derwin(win_h-border*2, win_w-border*2, border, border)
 
         while True:
             Field(win_w-1, win_h-1).curses(win)
@@ -39,4 +47,4 @@ class ScreensaverCommand(Command):
             time.sleep(.25)
 
     def run(self, options, args):
-        curses.wrapper(self.cursesScreensaver)
+        curses.wrapper(self.cursesScreensaver, options, args)

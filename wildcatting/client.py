0dde3a6839e112a9563c7b8e956d14fce0e04ba8
healthbystunts <bob@9r33n.net>
2007-11-19 15:38:34 +0000

    Woo bounds checking.

--- wildcatting/client.py
+++ wildcatting/client.py
@@ -21,26 +21,28 @@ class client:
     def border(self):
         (h,w) = self._stdscr.getmaxyx()
         self._stdscr.addstr(1, 3, "Let's go wildcatting!\n", curses.A_REVERSE)
-        self._field_win.box()
-        self._field_win.refresh()
+        self._border_win.box()
+        self._border_win.refresh()
                     
 
     def wildcatting(self, stdscr):
         self._stdscr = stdscr
         
         (h,w) = stdscr.getmaxyx()
-        field_h = h - 4
-        field_w = w - 6
+        border_h = h - 4
+        border_w = w - 6
+        field_w = border_w - 2
+        field_h = border_h - 2
         self._oilfield = OilField(field_w, field_h)
         self._playerfield = PlayerField(field_w, field_h)
-
-        self._field_win = stdscr.derwin(field_h, field_w, 2, 3)
+        self._border_win = stdscr.derwin(border_h, border_w, 2, 3) 
+        self._field_win = stdscr.derwin(field_h, field_w, 3, 4)
         self._field_win.keypad(1)
         self.border()
 
         view = FieldView(self._field_win, self._playerfield)
 
-        x = 1 ; y = 1
+        x = 0 ; y = 0
         
         self._field_win.addch(y, x, " ", curses.A_REVERSE)
         self._field_win.refresh()
@@ -48,6 +50,7 @@ class client:
         while True:
             curses.curs_set(0)
             dx = 0 ; dy = 0
+            survey = False
             c = stdscr.getch()
             if c == curses.KEY_UP: dy = -1
             elif c == curses.KEY_DOWN: dy = 1
@@ -55,18 +58,25 @@ class client:
             elif c == curses.KEY_RIGHT: dx = 1
             elif c == curses.KEY_MOUSE:
                 mid, mx, my, mz, bstate = curses.getmouse()
-                dx = mx - x - 3
-                dy = my - y - 2
-                self.survey(mx-3, my-2)
+                dx = mx - x - 4
+                dy = my - y - 3
+                survey = True
             elif c == ord(' '): 
-                self.survey(x, y)
+                survey = True
 
             if dx != 0 or dy != 0:
+                if (x + dx) > field_w-1 or (y + dy) > field_h-1 or \
+                    (x + dx) < 0 or (y + dy) < 0:
+                    continue
+                
                 self._field_win.addch(y, x, " ", curses.color_pair(0))
                 x += dx ; y += dy
                 self._field_win.addstr(y, x, " ")
                 self._field_win.move(y, x)
                 self._field_win.refresh()
+
+            if survey:
+                self.survey(x, y)
 
             self._field_win.addch(y, x, " ", curses.A_REVERSE)
             view.display()

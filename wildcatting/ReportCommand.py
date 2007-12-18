dd6978283554ae9cde65b33bd0f734ce2efebe9b
healthbystunts <bob@9r33n.net>
2007-12-18 20:26:49 +0000

    Temporary test command

---
+++ wildcatting/ReportCommand.py
@@ -0,0 +1,22 @@
+import logging
+import socket
+import sys
+import os
+import curses
+from cmdparse import Command
+
+import wildcatting.util
+import wildcatting.report
+
+
+from xmlrpclib import ServerProxy
+
+class ReportCommand(Command):
+    log = logging.getLogger("Wildcatting")
+
+    def __init__(self):
+        Command.__init__(self, "report", summary="Test a Wildcatting report")
+
+    def run(self, options, args):
+        wildcatting.util.startLogger("report.log")
+        curses.wrapper(wildcatting.report.main)

b4201d87fbc0abfcf9e6e342006afb54386693f4
healthbystunts <bob@9r33n.net>
2007-12-19 20:29:33 +0000

    Add west texas theme

--- test/wildcatting/model/test_serialize.py
+++ test/wildcatting/model/test_serialize.py
@@ -1,6 +1,6 @@
 import unittest
 
-from wildcatting.model import OilField, Site
+from wildcatting.model import OilField, Site, Setting
 
 class TestSerializer(unittest.TestCase):
     def testSite(self):
@@ -16,6 +16,14 @@ class TestSerializer(unittest.TestCase):
         obj1 = field1.serialize()
         field2 = OilField.deserialize(obj1)
         obj2 = field2.serialize()
+
+        self.assertEqual(obj1, obj2)
+
+    def testSetting(self):
+        setting1 = Setting()
+        obj1 = setting1.serialize()
+        setting2 = Setting.deserialize(obj1)
+        obj2 = setting2.serialize()
 
         self.assertEqual(obj1, obj2)
         

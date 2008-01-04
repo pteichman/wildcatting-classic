b97d0f7fb3671f81406674a011977c4376e08a6e
Peter Teichman <peter@teichman.org>
2008-01-04 14:02:04 +0000

    add the test suite to the tarball

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name="wildcatting",
       version="0.1",
-      scripts=["go-wildcatting"],
+      scripts=["go-wildcatting", "test/suite"],
       packages=["wildcatting",
                 "wildcatting.model",
                 "wildcatting.theme",
